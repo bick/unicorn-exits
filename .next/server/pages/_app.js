0174a076cb13db480eb8e842a08ce2bbbddf1513
Owen Bick <owenbick@gmail.com>
2022-09-02 23:06:38 -0400

    Initial upload

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,97 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"MyApp\": () => (/* binding */ MyApp),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_global_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/global.scss */ \"./styles/global.scss\");\n/* harmony import */ var _styles_global_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_global_scss__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _styles_992up_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/_992up.scss */ \"./styles/_992up.scss\");\n/* harmony import */ var _styles_992up_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_992up_scss__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _styles_1440up_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../styles/_1440up.scss */ \"./styles/_1440up.scss\");\n/* harmony import */ var _styles_1440up_scss__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_1440up_scss__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _styles_grid_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../styles/grid.scss */ \"./styles/grid.scss\");\n/* harmony import */ var _styles_grid_scss__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_styles_grid_scss__WEBPACK_IMPORTED_MODULE_4__);\n/* harmony import */ var _styles_typography_scss__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../styles/typography.scss */ \"./styles/typography.scss\");\n/* harmony import */ var _styles_typography_scss__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_styles_typography_scss__WEBPACK_IMPORTED_MODULE_5__);\n\n\n\n\n\n\nfunction MyApp({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/work/Documents/Unicorn Exits/unicorn-exits/pages/_app.js\",\n        lineNumber: 8,\n        columnNumber: 10\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUE7QUFBOEI7QUFDQTtBQUNDO0FBQ0g7QUFDTTtBQUUzQixTQUFTQSxLQUFLLENBQUMsRUFBRUMsU0FBUyxHQUFFQyxTQUFTLEdBQUUsRUFBRTtJQUM5QyxxQkFBTyw4REFBQ0QsU0FBUztRQUFFLEdBQUdDLFNBQVM7Ozs7O1lBQUksQ0FBQztDQUNyQztBQUVELGlFQUFlRixLQUFLIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbGFuZGluZy8uL3BhZ2VzL19hcHAuanM/ZTBhZCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgJy4uL3N0eWxlcy9nbG9iYWwuc2NzcydcbmltcG9ydCAnLi4vc3R5bGVzL185OTJ1cC5zY3NzJ1xuaW1wb3J0ICcuLi9zdHlsZXMvXzE0NDB1cC5zY3NzJ1xuaW1wb3J0ICcuLi9zdHlsZXMvZ3JpZC5zY3NzJ1xuaW1wb3J0ICcuLi9zdHlsZXMvdHlwb2dyYXBoeS5zY3NzJ1xuXG5leHBvcnQgZnVuY3Rpb24gTXlBcHAoeyBDb21wb25lbnQsIHBhZ2VQcm9wcyB9KSB7XG4gIHJldHVybiA8Q29tcG9uZW50IHsuLi5wYWdlUHJvcHN9IC8+O1xufVxuXG5leHBvcnQgZGVmYXVsdCBNeUFwcFxuIl0sIm5hbWVzIjpbIk15QXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles/_1440up.scss":
+/*!*****************************!*\
+  !*** ./styles/_1440up.scss ***!
+  \*****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./styles/_992up.scss":
+/*!****************************!*\
+  !*** ./styles/_992up.scss ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./styles/global.scss":
+/*!****************************!*\
+  !*** ./styles/global.scss ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./styles/grid.scss":
+/*!**************************!*\
+  !*** ./styles/grid.scss ***!
+  \**************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./styles/typography.scss":
+/*!********************************!*\
+  !*** ./styles/typography.scss ***!
+  \********************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
+module.exports = __webpack_exports__;
+
+})();
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import { SiTwitter } from "react-icons/si";
 import { SiTiktok } from "react-icons/si";
 import { HiOutlineMail } from "react-icons/hi";
 
-
 export default function Home() {
   return (
     <>
@@ -24,25 +23,29 @@ export default function Home() {
       <header className="header">
         <div class="header__brand">
           <Link href="/">
-            <a>
-            🦄
-            </a>
+            <a>🦄</a>
           </Link>
         </div>
         <ul className="header__list">
           <li class="header__item">
             <Link href="spotify.com">
-              <a><img src="/spotify.svg" class="podcast-network" /></a>
+              <a target="_blank" rel="nofollow">
+                <img src="/spotify.svg" class="podcast-network" />
+              </a>
             </Link>
           </li>
           <li class="header__item">
             <Link href="apple.com">
-              <a><img src="/apple.svg" class="podcast-network" /></a>
+              <a target="_blank" rel="nofollow">
+                <img src="/apple.svg" class="podcast-network" />
+              </a>
             </Link>
           </li>
           <li class="header__item">
             <Link href="google.com">
-              <a><img src="/google.svg" class="podcast-network" /></a>
+              <a target="_blank" rel="nofollow">
+                <img src="/google.svg" class="podcast-network" />
+              </a>
             </Link>
           </li>
         </ul>
@@ -58,23 +61,24 @@ export default function Home() {
         <footer className="footer home">
           <ul className="footer__list">
             <li className="footer__item">
-              <Link href="mailto:hello@weekendlabs.net">
-                <a className="foorer__link"><SiInstagram></SiInstagram></a>
+              <Link href="https://instagram.com/unicornexits">
+                <a className="foorer__link" target="_blank" rel="nofollow">
+                  <SiInstagram></SiInstagram>
+                </a>
               </Link>
             </li>
             <li className="footer__item">
-              <Link href="mailto:hello@weekendlabs.net">
-                <a className="foorer__link"><SiTwitter></SiTwitter></a>
+              <Link href="https://twitter.com/unicornexits">
+                <a className="foorer__link" target="_blank" rel="nofollow">
+                  <SiTwitter></SiTwitter>
+                </a>
               </Link>
             </li>
             <li className="footer__item">
-              <Link href="mailto:hello@weekendlabs.net">
-                <a className="foorer__link"><SiTiktok></SiTiktok></a>
-              </Link>
-            </li>
-            <li className="footer__item">
-              <Link href="mailto:hello@weekendlabs.net">
-                <a className="foorer__link"><HiOutlineMail></HiOutlineMail></a>
+              <Link href="https://tiktok.com/unicornexits">
+                <a className="foorer__link" target="_blank" rel="nofollow">
+                  <SiTiktok></SiTiktok>
+                </a>
               </Link>
             </li>
           </ul>
